2ccbf14c0cd0435810f12d20721a50baf8266403
Ian Murphy <immur.50ae@gmail.com>
2016-05-06 12:41:59 -0700

    Camera works

---
+++ Assets/Puzzle6/WebcamOnTexture.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WebcamOnTexture : MonoBehaviour {
+
+	public UnityEngine.UI.RawImage rawimage;
+
+	void Start () {
+		WebCamTexture webcamTexture = new WebCamTexture();
+		rawimage.texture = webcamTexture;
+		rawimage.material.mainTexture = webcamTexture;
+		webcamTexture.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
